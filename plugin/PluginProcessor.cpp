b2e02be82196db816bcbafb3e615cda43c2854a9
benson <freed602om@gmail.com>
2020-09-15 20:38:23 +0800

    combine original audio engine with PluginProcessor. WIP

--- plugin/PluginProcessor.cpp
+++ plugin/PluginProcessor.cpp
@@ -1,5 +1,7 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+#include "../audio_engine/FastSin.h"
+#include "../audio_engine/AudioLib/ValueTables.h"
 
 //==============================================================================
 AudioPluginAudioProcessor::AudioPluginAudioProcessor()
@@ -10,8 +12,107 @@ AudioPluginAudioProcessor::AudioPluginAudioProcessor()
                       #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
-                       )
+                       ), reverb(44100)
 {
+    AudioLib::ValueTables::Init();
+	CloudSeed::FastSin::Init();
+  /*  
+  reverb.SetParameter(Parameter::InputMix, 0.0);
+  reverb.SetParameter(Parameter::PreDelay, 0.0);
+  reverb.SetParameter(Parameter::HighPass, 0.0);
+  reverb.SetParameter(Parameter::LowPass, 0.755000114440918);
+  reverb.SetParameter(Parameter::TapCount, 0.41499990224838257);
+  reverb.SetParameter(Parameter::TapLength, 0.43999996781349182);
+  reverb.SetParameter(Parameter::TapGain, 0.87999999523162842);
+  reverb.SetParameter(Parameter::TapDecay, 1.0);
+  reverb.SetParameter(Parameter::DiffusionEnabled, 1.0);
+  reverb.SetParameter(Parameter::DiffusionStages, 0.71428573131561279);
+  reverb.SetParameter(Parameter::DiffusionDelay, 0.335000216960907);
+  reverb.SetParameter(Parameter::DiffusionFeedback, 0.660000205039978);
+  reverb.SetParameter(Parameter::LineCount, 0.18181818723678589);
+  reverb.SetParameter(Parameter::LineDelay, 0.51000016927719116);
+  reverb.SetParameter(Parameter::LineDecay, 0.29999998211860657);
+  reverb.SetParameter(Parameter::LateDiffusionEnabled, 1.0);
+  reverb.SetParameter(Parameter::LateDiffusionStages, 0.4285714328289032);
+  reverb.SetParameter(Parameter::LateDiffusionDelay, 0.22999951243400574);
+  reverb.SetParameter(Parameter::LateDiffusionFeedback, 0.59499990940093994);
+  reverb.SetParameter(Parameter::PostLowShelfGain, 0.87999987602233887);
+  reverb.SetParameter(Parameter::PostLowShelfFrequency, 0.19499993324279785);
+  reverb.SetParameter(Parameter::PostHighShelfGain, 0.875);
+  reverb.SetParameter(Parameter::PostHighShelfFrequency, 0.59000009298324585);
+  reverb.SetParameter(Parameter::PostCutoffFrequency, 0.79999983310699463);
+  reverb.SetParameter(Parameter::EarlyDiffusionModAmount, 0.13499999046325684);
+  reverb.SetParameter(Parameter::EarlyDiffusionModRate, 0.29000008106231689);
+  reverb.SetParameter(Parameter::LineModAmount, 0.18999995291233063);
+  reverb.SetParameter(Parameter::LineModRate, 0.22999987006187439);
+  reverb.SetParameter(Parameter::LateDiffusionModAmount, 0.1249999925494194);
+  reverb.SetParameter(Parameter::LateDiffusionModRate, 0.28500008583068848);
+  reverb.SetParameter(Parameter::TapSeed, 0.00048499999684281647);
+  reverb.SetParameter(Parameter::DiffusionSeed, 0.00020799999765586108);
+  reverb.SetParameter(Parameter::DelaySeed, 0.00033499998971819878);
+  reverb.SetParameter(Parameter::PostDiffusionSeed, 0.00037200000951997936);
+  reverb.SetParameter(Parameter::CrossSeed, 0.42500001192092896);
+  reverb.SetParameter(Parameter::DryOut, 1.0);
+  reverb.SetParameter(Parameter::PredelayOut, 0.0);
+  reverb.SetParameter(Parameter::EarlyOut, 0.8599998950958252);
+  reverb.SetParameter(Parameter::MainOut, 0.90500003099441528);
+  reverb.SetParameter(Parameter::HiPassEnabled, 0.0);
+  reverb.SetParameter(Parameter::LowPassEnabled, 1.0);
+  reverb.SetParameter(Parameter::LowShelfEnabled, 0.0);
+  reverb.SetParameter(Parameter::HighShelfEnabled, 0.0);
+  reverb.SetParameter(Parameter::CutoffEnabled, 0.0);
+  reverb.SetParameter(Parameter::LateStageTap, 1.0);
+  reverb.SetParameter(Parameter::Interpolation, 1.0);
+  */
+
+  reverb.SetParameter(Parameter::InputMix, 0.0);
+  reverb.SetParameter(Parameter::PreDelay, 0.0);
+  reverb.SetParameter(Parameter::HighPass, 0.0);
+  reverb.SetParameter(Parameter::LowPass, 0.74000012874603271);
+  reverb.SetParameter(Parameter::TapCount, 1.0);
+  reverb.SetParameter(Parameter::TapLength, 1.0);
+  reverb.SetParameter(Parameter::TapGain, 1.0);
+  reverb.SetParameter(Parameter::TapDecay, 0.71000003814697266);
+  reverb.SetParameter(Parameter::DiffusionEnabled, 1.0);
+  reverb.SetParameter(Parameter::DiffusionStages, 1.0);
+  reverb.SetParameter(Parameter::DiffusionDelay, 0.65999996662139893);
+  reverb.SetParameter(Parameter::DiffusionFeedback, 0.76000010967254639);
+  reverb.SetParameter(Parameter::LineCount, 1.0);
+  reverb.SetParameter(Parameter::LineDelay, 0.9100002646446228);
+  reverb.SetParameter(Parameter::LineDecay, 0.80999958515167236);
+  reverb.SetParameter(Parameter::LateDiffusionEnabled, 1.0);
+  reverb.SetParameter(Parameter::LateDiffusionStages, 1.0);
+  reverb.SetParameter(Parameter::LateDiffusionDelay, 0.71499955654144287);
+  reverb.SetParameter(Parameter::LateDiffusionFeedback, 0.71999979019165039);
+  reverb.SetParameter(Parameter::PostLowShelfGain, 0.87999987602233887);
+  reverb.SetParameter(Parameter::PostLowShelfFrequency, 0.19499993324279785);
+  reverb.SetParameter(Parameter::PostHighShelfGain, 0.72000008821487427);
+  reverb.SetParameter(Parameter::PostHighShelfFrequency, 0.520000159740448);
+  reverb.SetParameter(Parameter::PostCutoffFrequency, 0.7150002121925354);
+  reverb.SetParameter(Parameter::EarlyDiffusionModAmount, 0.41999998688697815);
+  reverb.SetParameter(Parameter::EarlyDiffusionModRate, 0.30500012636184692);
+  reverb.SetParameter(Parameter::LineModAmount, 0.4649999737739563);
+  reverb.SetParameter(Parameter::LineModRate, 0.3199998140335083);
+  reverb.SetParameter(Parameter::LateDiffusionModAmount, 0.40999993681907654);
+  reverb.SetParameter(Parameter::LateDiffusionModRate, 0.31500011682510376);
+  reverb.SetParameter(Parameter::TapSeed, 0.0003009999927598983);
+  reverb.SetParameter(Parameter::DiffusionSeed, 0.00018899999849963933);
+  reverb.SetParameter(Parameter::DelaySeed, 0.0001610000035725534);
+  reverb.SetParameter(Parameter::PostDiffusionSeed, 0.00050099997315555811);
+  reverb.SetParameter(Parameter::CrossSeed, 1.0);
+  reverb.SetParameter(Parameter::DryOut, 0.0);
+  reverb.SetParameter(Parameter::PredelayOut, 0.0);
+  reverb.SetParameter(Parameter::EarlyOut, 0.0);
+  reverb.SetParameter(Parameter::MainOut, 0.95499974489212036);
+  reverb.SetParameter(Parameter::HiPassEnabled, 0.0);
+  reverb.SetParameter(Parameter::LowPassEnabled, 1.0);
+  reverb.SetParameter(Parameter::LowShelfEnabled, 0.0);
+  reverb.SetParameter(Parameter::HighShelfEnabled, 0.0);
+  reverb.SetParameter(Parameter::CutoffEnabled, 1.0);
+  reverb.SetParameter(Parameter::LateStageTap, 1.0);
+  reverb.SetParameter(Parameter::Interpolation, 1.0);
+
+
 }
 
 AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
@@ -136,6 +237,8 @@ void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
+    for (auto i = 2; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
 
     // This is the place where you'd normally do the guts of your plugin's
     // audio processing...
@@ -143,12 +246,10 @@ void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     // the samples and the outer loop is handling the channels.
     // Alternatively, you can process the samples with the channels
     // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
-    }
+    
+        const float** in_sig =  buffer.getArrayOfReadPointers();
+        float** out_sig =  buffer.getArrayOfWritePointers();
+        reverb.Process(in_sig,out_sig, buffer.getNumSamples());
 }
 
 //==============================================================================
--- plugin/PluginProcessor.h
+++ plugin/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <juce_audio_processors/juce_audio_processors.h>
+#include "../audio_engine/ReverbController.h"
 
 //==============================================================================
 class AudioPluginAudioProcessor  : public juce::AudioProcessor
@@ -42,6 +43,7 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+	CloudSeed::ReverbController reverb;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
 };
